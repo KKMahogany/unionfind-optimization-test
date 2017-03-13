3c2f8f665c9545bae796a414d6211f2343205aa9
Kevin Tran <kevinohappyface@gmail.com>
2017-03-13 22:01:57 +1100

    Make test.sh's output a bit nicer

--- test.sh
+++ test.sh
@@ -5,6 +5,14 @@ make
 queries=$((5*10**6))
 seed=312876
 
+echo "Path compression ONLY"
 time ./uf1 $queries $queries $seed
+echo
+
+echo "Union by rank ONLY"
 time ./uf2 $queries $queries $seed
+echo
+
+echo "Path compression WITH union by (approximate) rank"
 time ./uf3 $queries $queries $seed
+echo
